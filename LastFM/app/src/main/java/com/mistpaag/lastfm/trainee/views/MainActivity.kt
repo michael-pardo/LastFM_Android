566d237ae002a587388a6d9c28eb4de5996b13a5
Michael Pardo Imac <michael.stiver.pardo@gmail.com>
2020-08-25 14:59:30 -0500

    create android studio project

---
+++ LastFM/app/src/main/java/com/mistpaag/lastfm/trainee/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mistpaag.lastfm.trainee.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mistpaag.lastfm.trainee.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
